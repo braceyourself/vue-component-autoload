6809e1e2cde80edbbd9fad6b2e231bb75f71a875
Ethan Brace <ethanabrace@gmail.com>
2019-08-20 05:44:15 -0400

    make it a vue plugin...

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-import Vue from 'vue';
 import _ from 'lodash';
-let component_directory = './components';
 
-const files = require.context(component_directory, true, /\.vue$/i);
-	files.keys().map(key => {
-	let name = _.kebabCase(key.split('/').pop().split('.')[0]);
-	let component = files(key).default;
-	Vue.component(name, component);
+const loader {
+	install(Vue, options){
+		let component_directory = options.dir;
+		if(component_directory === undefined){
+
+			component_directory = './components';
+		}
+
+		const files = require.context(component_directory, true, /\.vue$/i);
+			files.keys().map(key => {
+			let name = _.kebabCase(key.split('/').pop().split('.')[0]);
+			let component = files(key).default;
+			Vue.component(name, component);
+		}
+
+		
+	}
+}
+
+export default loader;
+
+if(typeof window !== 'undefined' && window.Vue){
+	window.Vue.use(loader);
 }
