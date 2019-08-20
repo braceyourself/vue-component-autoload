7b869e32f7d5352561ea9bb3d90b22c0ad363c0b
Ethan Brace <ethanabrace@gmail.com>
2019-08-20 06:47:27 -0400

    add logging

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import _ from 'lodash'
 
 const loader = {
+    log(...args) {
+        if (this.logging) {
+            console.log(...args);
+        }
+    },
     install(Vue, options) {
         if (options === undefined) {
             options = {};
@@ -8,13 +13,16 @@ const loader = {
 
         let opts = Object.assign(options,{
             dir:'./components',
+            logging: true
         });
 
+        this.logging = opts.logging;
 
         const files = require.context(opts.dir, true, /\.vue$/i);
         files.keys().map(key => {
             let name = _.kebabCase(key.split('/').pop().split('.')[0]);
             let component = files(key).default;
+            log("[Auto-loading Component]",name, component);
             Vue.component(name, component);
         })
 
