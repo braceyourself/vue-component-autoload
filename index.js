cb6fec847e2c286a2ff6d74f2b06c7ecd3693a99
Ethan Brace <ethanabrace@gmail.com>
2019-08-20 05:24:51 -0400

    run on require

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 import Vue from 'vue';
 import _ from 'lodash';
+let component_directory = './components';
 
-module.exports = function(dir){
-	const files = require.context(dir, true, /\.vue$/i);
+const files = require.context(component_directory, true, /\.vue$/i);
 	files.keys().map(key => {
-		let name = _.kebabCase(key.split('/').pop().split('.')[0]);
-		let component = files(key).default;
-		Vue.component(name, component);
-	}
+	let name = _.kebabCase(key.split('/').pop().split('.')[0]);
+	let component = files(key).default;
+	Vue.component(name, component);
 }
